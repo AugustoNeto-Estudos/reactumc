0ff3c81b8eb668c507fa0b6ac0b30cb0f17d8c3c
AugustoNeto-Estudos <11221101575@alunos.umc.br>
2025-11-11 20:41:40 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React - Hello World! Augusto Corrêa</h1>
+      <h1>Vite + React - Hello World! Augusto Corrêa e Nicollas Cavalcante</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
